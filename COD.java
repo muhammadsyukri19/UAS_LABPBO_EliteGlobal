ea255005d1fd2af8229608aa44d30b337b8e5a47
SidqiAlfareza <sidqi23@mhs.usk.ac.id>
2024-12-01 15:56:07 +0700

    COD.java

--- COD.java
+++ COD.java
@@ -2,7 +2,16 @@ class COD extends Pembayaran {
 
     @Override
     void prosesPembayaran() {
-        System.out.println("Pembayaran tunai (Cash on Delivery/COD) sedang diproses...");
+        System.out.print("Memproses pembayaran...");
+        try {
+            for (int i = 0; i < 5; i++) {
+                Thread.sleep(500); // Delay 500 ms
+                System.out.print(".");
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("\nPembayaran tunai (Cash on Delivery/COD) sedang diproses...");
     }
 
     @Override
